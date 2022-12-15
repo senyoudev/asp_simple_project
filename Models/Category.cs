d7a0727a0aa2425af78e22b25b2437efc2b96299
senyoudev <younesmeskafe2018@gmail.com>
2022-12-15 16:43:08 +0100

    modification

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,5 +2,10 @@
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        
     }
 }
