f49a81e1df08168d176e4b554a50367766153503
senyoudev <younesmeskafe2018@gmail.com>
2022-12-17 20:20:57 +0100

    category controller

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using freecodecamp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace freecodecamp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace freecodecamp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace freecodecamp.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
