6a71f77128126e424b13a0c2d4cceb013ff55683
senyoudev <younesmeskafe2018@gmail.com>
2022-12-18 01:02:00 +0100

    create & view

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using freecodecamp.Data;
+using freecodecamp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace freecodecamp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            IEnumerable<Category> objCategories = _db.Categories.ToList();
+            return View(objCategories);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
             return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
